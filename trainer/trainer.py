bcba56e6cafdefba94d333b686e8785be71a6bf2
thegenerativegeneration <philipp.haslbauer@hslu.ch>
2024-07-02 12:45:04 +0200

    add depth_anything_v2

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -144,6 +144,39 @@ class GaussianTrainer(object):
                 PrepareForNet(),
             ])
             self.depth_model = depth_anything
+
+        elif self.depth_model_type == "depth_anything_v2":
+            from torchvision.transforms import Compose
+            from depth_anything_v2.dpt import DepthAnythingV2
+            from depth_anything_v2.util.transform import Resize, NormalizeImage, PrepareForNet
+            encoder = 'vits'
+            size_map = {'vits': "Small", 'vitb': "Base", 'vitl': "Large", 'vitg': "Giant"}
+            model_configs = {
+                'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+                'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+                'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+                'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+            }
+            model_config = model_configs[encoder]
+            size = size_map[encoder]
+            url = f"https://huggingface.co/depth-anything/Depth-Anything-V2-{size}/resolve/main/depth_anything_v2_{encoder}.pth?download=true"
+            depth_anything = DepthAnythingV2(**model_config).eval()
+            depth_anything.load_state_dict(torch.hub.load_state_dict_from_url(url))
+
+            self.depth_transforms = Compose([
+                Resize(
+                    width=518,
+                    height=518,
+                    resize_target=False,
+                    keep_aspect_ratio=True,
+                    ensure_multiple_of=14,
+                    resize_method='lower_bound',
+                    image_interpolation_method=cv2.INTER_CUBIC,
+                ),
+                NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                PrepareForNet(),
+            ])
+            self.depth_model = depth_anything
         else:
             midas = torch.hub.load("intel-isl/MiDaS", "DPT_Hybrid")
             midas.to(device)
@@ -157,7 +190,7 @@ class GaussianTrainer(object):
         if self.depth_model_type == "zoe":
             depth = self.depth_model.infer_pil(Image.fromarray(img.astype(np.uint8)),
                                                output_type='tensor')
-        elif self.depth_model_type == "depth_anything":
+        elif self.depth_model_type in ["depth_anything", "depth_anything_v2"]:
             image = self.depth_transforms({'image': img/255.})['image']
             image = torch.from_numpy(image).unsqueeze(0)
             # depth shape: 1xHxW
@@ -178,6 +211,7 @@ class GaussianTrainer(object):
             depth = scale * prediction + shift
             depth[depth < 1e-8] = 1e-8
             depth = 1.0 / depth
+        
         else:
             device = "cuda" if torch.cuda.is_available() else "cpu"
             input_batch = self.depth_transforms(img).to(device)
